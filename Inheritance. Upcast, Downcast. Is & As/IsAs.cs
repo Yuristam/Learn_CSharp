6bda292a8906b9f8aad68a33905ae9ce8f84c871
Yuristam <yuristamasanakunov@gmail.com>
2025-05-09 00:39:30 +0600

    Inheritance. Upcast & downcast. is as

---
+++ Inheritance. Upcast, Downcast. Is & As/IsAs.cs
@@ -0,0 +1,97 @@
+﻿// "is" keyword
+Builder john = new Builder();
+Person man = john;
+
+if (man is Builder builder)
+{
+    builder.Build();
+}
+
+
+
+// "as" keyword
+Driver driver = man as Driver;
+
+if (driver != null)
+{
+    driver.Drive();
+}
+
+
+
+
+
+// "is" keyword
+Animal pet = new Dog();
+
+if (pet is Dog)
+{
+    Console.WriteLine("It's a dog!");
+    pet.Speak();  // Output: Woof!
+}
+
+// C# 7+: pattern matching with type cast
+if (pet is Dog rex)
+{
+    rex.Speak(); // Output: Woof!
+}
+/// Checks if an object is of a given type (or can be cast to that type). It returns a boolean.
+/// great for CHECKING THE TYPE
+
+
+
+
+
+// "as" keyword
+Animal newPet = new Cat();
+
+Dog dog = newPet as Dog;
+
+if (dog != null)
+{
+    dog.Speak(); // Won't run because pet is actually a Cat
+}
+else
+{
+    Console.WriteLine("pet is not a Dog"); // Output: pet is not a Dog
+}
+/// Attempts to cast an object to a specific type. Returns null if the cast fails, instead of throwing an exception
+/// used for SAFE CASTING
+
+
+
+
+
+class Animal
+{
+    public virtual void Speak() => Console.WriteLine("Animal sound");
+}
+
+class Dog : Animal
+{
+    public override void Speak() => Console.WriteLine("Woof!");
+}
+
+class Cat : Animal
+{
+    public override void Speak() => Console.WriteLine("Meow!");
+}
+
+
+
+
+
+class Person
+{
+    public void DoWork() => Console.WriteLine("Working");
+}
+
+class Driver : Person
+{
+    public void Drive() => Console.WriteLine("Driving");
+}
+
+class Builder : Person
+{
+    public void Build() => Console.WriteLine("Building");
+}
---
+++ Inheritance. Upcast, Downcast. Is & As/Program.cs
@@ -0,0 +1,52 @@
+﻿Father father = new Father();
+father.Name = "John";
+father.Age = 50;
+father.SayHello();
+father.DoWork();
+
+// Console.WriteLine(father.wallet);  // Error: can't access to the private field 
+// Console.WriteLine(father.money);   // Error: can't access to the protected field 
+
+
+/// Upcast - converting "child" class into "parent" class
+Grandfather man = father;
+// man.DoWork(); // Error: we can't use "child" class method 
+
+
+/// Downcast - converting "parent" class into "child" class
+Father dad = (Father)man;
+
+
+class Grandfather
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine("How do you do?");
+    }
+}
+
+class Father : Grandfather
+{
+    private Wallet wallet = new Wallet();  // only this class can access it (because it's private)
+    protected Money money = new Money();   // only this class and "child" classes can access it (because it's protected)
+
+    public void DoWork()
+    {
+        Console.WriteLine("Working");
+    }
+}
+
+class Child : Father
+{
+    public void SpendMoney()
+    {
+        Console.WriteLine("Spending " + money);  // "money"(variable) from "parent" class
+                                                 // we can't use wallet here, because it's private in "parent" class
+    }
+}
+
+class Wallet { }
+class Money { }
